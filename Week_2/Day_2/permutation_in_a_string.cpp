35f85ac9b9e1bdf6c4e638c7ceb0dd71d90ebd73
Satyam <168332150+Sa1yam-Singh@users.noreply.github.com>
2026-06-09 22:56:46 +0530

    Update permutation_in_a_string.cpp

--- Week_2/Day_2/permutation_in_a_string.cpp
+++ Week_2/Day_2/permutation_in_a_string.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-    int n1 = s1.size();
-    int n2 = s2.size();
-    if (n1 > n2) {
+    int n1=s1.size();
+    int n2=s2.size();
+    if (n1>n2) {
         return false;
     }
     vector<int> hash1(26, 0);
     vector<int> hash2(26, 0);
-    for (int i = 0; i < n1; i++) {
-        hash1[s1[i] - 'a']++;
-        hash2[s2[i] - 'a']++;
+    for (int i=0;i<n1;i++) {
+        hash1[s1[i]-'a']++;
+        hash2[s2[i]-'a']++;
     }
-    if (hash1 == hash2) {
+    if (hash1==hash2) {
         return true;
     }
-    for (int i = n1; i < n2; i++) {
-        hash2[s2[i] - 'a']++;
-        hash2[s2[i - n1] - 'a']--;
-        if (hash1 == hash2) {
+    for (int i=n1;i<n2;i++) {
+        hash2[s2[i]-'a']++;
+        hash2[s2[i-n1]-'a']--;
+        if (hash1==hash2) {
             return true;
         }
     }
