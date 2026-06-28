16aa273539e44b7d0a6fb97b8401f4cf713465b3
Satyam Singh <168332150+Sa1yam-Singh@users.noreply.github.com>
2026-06-28 19:23:59 +0530

    Create point.java

---
+++ Week_4/OOPS/point.java
@@ -0,0 +1,21 @@
+class point{
+    private int x,y;
+    public point(){
+        this.x=0;
+        this.y=0;
+    }
+    public point(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+    public void setX(int x){
+        this.x=x;
+    }
+    public void setY(int y){
+        this.y=y;
+    }
+    public void setXY(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+}
